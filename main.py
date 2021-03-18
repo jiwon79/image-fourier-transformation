487efb7f9653c735e49add629ddb217905485e62
jiwon79 <hello662200@gmail.com>
2021-03-18 16:29:14 +0900

    tkinter window

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("image fourier transformation")
+root.geometry("600x400+100+100")
+root.resizable(True, True)
+
+canvas = Canvas(root, width=400, height=400, bg="black", bd=2)
+canvas.pack()
+
+root.mainloop()
