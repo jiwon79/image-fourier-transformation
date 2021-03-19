9d45905f39bcb7c06b571247a0b60d555b55a919
jiwon79 <hello662200@gmail.com>
2021-03-19 10:53:26 +0900

    outline img to connected list

--- img_to_function.py
+++ img_to_function.py
@@ -3,7 +3,7 @@ from classes import *
 l = [Point(10,10), Point(100,50), Point(40,100), Point(80,90)]
 def draw_by_list(canvas, l):
     for i in range(len(l)-1):
-        canvas.create_line(l[i].x, l[i].y, l[i+1].x, l[i+1].y, fill="#476042", width=5)
+        canvas.create_line(l[i].x, l[i].y, l[i+1].x, l[i+1].y, fill="#476042", width=1)
 
 def segment_function(p1, p2, t): # 0 <= t <= 1
     return p1 + (p2-p1)*t
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import *
 from PIL import Image
 from PIL import ImageTk
 
+from classes import *
 from img_to_function import *
 
 ############################ tkinter window ############################
@@ -12,13 +13,19 @@ window.geometry("1000x400+100+100")
 window.resizable(False, False)
 
 ############################### functions ##############################
+connectList = []
 def convert_to_tkimage():
     global src
+    global connectList
     
     # color img -> gray sclae -> outline
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     outline = cv2.Canny(gray, 100, 255)
     # cv2.imshow("canny", outline)
+
+    connectList = connect_points(outline)
+    # print(connectList)
+    draw_by_list(canvas, connectList)
 
     img = Image.fromarray(outline)
     imgtk = ImageTk.PhotoImage(image=img)
@@ -26,10 +33,38 @@ def convert_to_tkimage():
     label.config(image=imgtk)
     label.image = imgtk
 
+def connect_points(outline):
+    pointList = []
+    for i in range(img_width):
+        for j in range(img_height):
+            if outline[i][j] == 255:
+                pointList.append(Point(j, i))
+    
+    connectList = [0]
+    # print(len(pointList))
+    while len(connectList) != len(pointList):
+        dis = 800
+        point = pointList[connectList[-1]]
+        for i in range(len(pointList)):
+            if not i in connectList and dis > point.getDistance(pointList[i]):
+                dis = point.getDistance(pointList[i])
+                near = i
+        connectList.append(near)
+        
+        per = len(connectList)/len(pointList)
+        print(per)
 
+    for i in range(len(connectList)):
+        connectList[i] = pointList[connectList[i]]
+    # print(connectList)
+   
+    return connectList
+
+# bilateral filter
 ############################## load img ###############################
 src = cv2.imread("./img/lion.jpg")
-src = cv2.resize(src, (400, 400))
+img_width, img_height = 200, 200
+src = cv2.resize(src, (img_width,  img_height))
 
 # transform opencv(BGR) to tkinter(RGB)
 img = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
@@ -40,11 +75,8 @@ imgtk = ImageTk.PhotoImage(image=img)
 
 
 ################################ GUI ###################################
-canvas = Canvas(window, width=400, height=400, bg="white", bd=2)
+canvas = Canvas(window, width=img_width, height=img_height, bg="white", bd=2)
 canvas.place(x=0, y=0)
-
-l = [Point(10,10), Point(100,50), Point(40,100), Point(80,90)]
-draw_by_list(canvas, l)
 
 label = Label(window, image=imgtk)
 label.place(x=400, y=0)
