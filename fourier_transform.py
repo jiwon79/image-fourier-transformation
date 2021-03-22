653f43c2af5b54a479da8967d87f34ccbf1aaa2f
Seunghyun Yeo <shyeo0517@gmail.com>
2021-03-22 10:20:30 +0900

    fourier update

--- fourier_transform.py
+++ fourier_transform.py
@@ -27,10 +27,10 @@ def complex_fourier_transform(x, y, N):
 
 # x = lambda t: img_function(t).x
 # y = lambda t: img_function(t).y
-x = lambda t: 50*cos(2*pi*t)+ 30*cos(6*pi*t) + 200
-y = lambda t: 100*sin(2*pi*t)+200
+x = lambda t: 50*cos(2*pi*t)+ 30*cos(6*pi*t) - 30*sin(4*pi*t) + 200
+y = lambda t: 50*sin(2*pi*t)+ 100*abs(t-0.5) + 200
 
-N = 10
+N = 5
 c = complex_fourier_transform(x, y, N)
 
 window = Tk()
@@ -43,27 +43,45 @@ canvas.pack()
 window.update()
 
 centers = dict()
+circles = dict()
+arrows = dict()
 centers[0] = 0
-tmp = convert(0, 0, abs(c[0]))
-canvas.create_oval(tmp[0], tmp[1], tmp[2], tmp[3], fill="")
 
 for i in range(1, N+1):
     centers[i] = centers[1-i] + c[1-i]
     tmp = convert(centers[i].real, centers[i].imag, abs(c[i]))
-    canvas.create_oval(tmp[0], tmp[1], tmp[2], tmp[3], fill="")
+    circles[i] = canvas.create_oval(tmp[0], tmp[1], tmp[2], tmp[3], fill="")
+    arrows[i] = canvas.create_line(centers[1-i].real, centers[1-i].imag, centers[i].real, centers[i].imag)
     
     centers[-i] = centers[i] + c[i]
     tmp = convert(centers[-i].real, centers[-i].imag, abs(c[-i]))
-    canvas.create_oval(tmp[0], tmp[1], tmp[2], tmp[3], fill="")
+    circles[-i] = canvas.create_oval(tmp[0], tmp[1], tmp[2], tmp[3], fill="")
+    arrows[-i] = canvas.create_line(centers[i].real, centers[i].imag, centers[-i].real, centers[-i].imag)
 
+position = centers[-N] + c[-N]
+arrows[N+1] = canvas.create_line(centers[-N].real, centers[-N].imag, position.real, position.imag)
 
-for k in range(1000):
-    t = k / 1000
-    position = sum(c[i] * (cos(2*pi*i*t)+sin(2*pi*i*t)*1j) for i in range(-10, 11))
+M = 300
+
+for k in range(M):
+    t = k / M
+    for i in range(1, N+1):
+        centers[i] = centers[1-i] + c[1-i] * (cos(2*pi*(1-i)*t) + 1j * sin(2*pi*(1-i)*t))
+        tmp = convert(centers[i].real, centers[i].imag, abs(c[i]))
+        canvas.coords(circles[i], tmp[0], tmp[1], tmp[2], tmp[3])
+        canvas.coords(arrows[i], centers[1-i].real, centers[1-i].imag, centers[i].real, centers[i].imag)
+        
+        centers[-i] = centers[i] + c[i] * (cos(2*pi*i*t) + 1j * sin(2*pi*i*t))
+        tmp = convert(centers[-i].real, centers[-i].imag, abs(c[-i]))
+        canvas.coords(circles[-i], tmp[0], tmp[1], tmp[2], tmp[3])
+        canvas.coords(arrows[-i], centers[i].real, centers[i].imag, centers[-i].real, centers[-i].imag)
+    
+    position = centers[-N] + c[-N] * (cos(2*pi*(-N)*t) + 1j * sin(2*pi*(-N)*t))
+    canvas.coords(arrows[N+1], centers[-N].real, centers[-N].imag, position.real, position.imag)
     canvas.create_oval(position.real, position.imag, position.real+1, position.imag+1, fill="blue")
     window.update()
 
-time.sleep(1.5)
+window.mainloop()
     
 
 
